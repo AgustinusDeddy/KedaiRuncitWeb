bc6058585b88db0ce694007cea6e6450179ef620
deddyarief <deddyarief1@gmail.com>
2018-09-19 21:10:19 +0800

    Remove vendor & dist

--- Core/Interfaces/IAppLogger.cs
+++ Core/Interfaces/IAppLogger.cs
@@ -3,6 +3,6 @@
     public interface IAppLogger<T>
     {
         void LogInformation(string message, params object[] args);
-        void LogWarning(string message, params object[] args);
+        void LogWarning(string message, params object[] args); 
     }
 }
---
+++ Infrastructure/Data/Repositories/Repository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entities;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+
+    }
+}
